547fd1ebe8ea6ffd517543d2d7745b8739f5d0fa
shwethaQS <55647175+shwethaQS@users.noreply.github.com>
2024-03-13 13:11:41 +0000

    new changes

---
+++ Firstnonrepeatingcharacter.java
@@ -0,0 +1,29 @@
+public class Firstnonrepeatingcharacter{
+
+public  static void main(String[] args){
+
+ String input = "programming";
+ System.out.println("Firstnonrepeatingcharacter: "+ nonrepeatingcharacter(input));
+
+}
+
+public static char nonrepeatingcharacter(String str){
+   
+
+    int[] ch = new int[256];
+
+    for(char c : str.toCharArray()){
+
+        ch[c]++;
+    }
+
+for(char c : str.toCharArray()){
+
+    if(ch[c]==1){
+        return c;
+    }
+}
+return '\0';
+
+}
+}
---
+++ Firstsmallestpositivenumber.java
@@ -0,0 +1,25 @@
+public class Firstsmallestpositivenumber {
+
+    public static void main(String[] args){ 
+  
+
+    int[] test1 = {-1,0,1,3};
+    System.out.println("missingposnumber: " + firstpositivenumber(test1));
+    }
+
+    public static int firstpositivenumber(int[] A) {
+
+        Arrays.sort(A);
+
+        int positivenumber = 1;
+        for (int value : A) {
+
+            if (value == positivenumber) {
+                positivenumber++;
+            }
+        }
+        return positivenumber;
+
+    }
+
+}
